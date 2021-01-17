0ca12da64447f4ba58ca219fb7d0796f6729e293
Developer Experience team at Tendermint <hello@tendermint.com>
2021-01-17 20:29:57 +0700

    Initialized with Starport

---
+++ x/zoef/handler.go
@@ -0,0 +1,24 @@
+package zoef
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/faddat/zoef/x/zoef/keeper"
+	"github.com/faddat/zoef/x/zoef/types"
+)
+
+// NewHandler ...
+func NewHandler(k keeper.Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+
+		switch msg := msg.(type) {
+		// this line is used by starport scaffolding # 1
+		default:
+			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
+			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
+		}
+	}
+}
---
+++ x/zoef/keeper/grpc_query.go
@@ -0,0 +1,7 @@
+package keeper
+
+import (
+	"github.com/faddat/zoef/x/zoef/types"
+)
+
+var _ types.QueryServer = Keeper{}
